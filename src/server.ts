3ca99ee64da7fff55c8c8d2e4eed4a8c70605e71
santiago <souto.santiago@academico.ifpb.edu.br>
2024-07-01 15:28:30 -0300

    build

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,18 @@
-import fastify from "fastify"
-import { userController } from "./controllers/userController";
-import { authController } from "./controllers/authController";
+import fastify from 'fastify';
+import { userController } from './controllers/userController';
+import { authController } from './controllers/authController';
 
-const server = fastify()
+const server = fastify();
 
-server.register(userController)
-server.register(authController)
+const port = process.env.PORT || 4000;
 
-server.listen({
-  port:3333
-}).then(()=>{
-  console.log('deu certo');
-  
-})
+server.register(userController);
+server.register(authController);
+
+server.listen({ port: Number(port) }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server is running at ${address}`);
+});
